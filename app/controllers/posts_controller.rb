3d63e7f6e2262e6af6bf78f741a8de0b0fd3f3c1
Haganmc <hagan_mc@outlook.com>
2025-05-20 07:45:33 -0500

    added bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def new
     @post = Post.new
   end
@@ -18,8 +21,32 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Post was successfully updated."
+    else
+      flash.now[:alert] = @post.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
+
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+ def authorize_user!
+    redirect_to posts_path, alert: "Not authorized." unless @post.user == current_user
+  end
   def post_params
     params.require(:post).permit(:title, :content)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [ :new, :create, :index ]
+  resources :posts, only: [ :new, :create, :index, :edit, :update, :destroy ]
   devise_for :users
   root "posts#index"
 end
